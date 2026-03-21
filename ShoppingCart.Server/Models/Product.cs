f42ff6e3569a6ff498273bdd75f7666ba79eb6b2
jenniferzhou988 <jennifer.zhou2005@gmail.com>
2026-03-20 21:01:30 -0400

    check in

--- ShoppingCart.Server/Models/Product.cs
+++ ShoppingCart.Server/Models/Product.cs
@@ -1,15 +1,12 @@
+using ShoppingCartAPI.Models;
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AngularApplication.Models
 {
-    public class Product
+    public class Product : GDCTEntityBase<int>
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
-
         [Required]
         [MaxLength(200)]
         public string ProductName { get; set; } = null!;
@@ -20,14 +17,5 @@ namespace AngularApplication.Models
         [MaxLength(1000)]
         public string? Description { get; set; }
 
-        public DateTime Created { get; set; }
-
-        [MaxLength(256)]
-        public string? CreatedBy { get; set; }
-
-        public DateTime? Modified { get; set; }
-
-        [MaxLength(256)]
-        public string? ModifiedBy { get; set; }
     }
 }
